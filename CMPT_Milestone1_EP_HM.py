5439d2bace13f04223ef4421312762d188461008
Erwin <uiopq2521@gmail.com>
2025-03-28 11:11:47 -0600

    add milestone 2 files

--- CMPT_Milestone1_EP_HM.py
+++ CMPT_Milestone1_EP_HM.py
@@ -4,6 +4,8 @@
 # -------------------------------
 
 import pickle
+from datetime import date
+from graphics4 import GraphWin
 
 
 class Shape:
@@ -391,6 +393,26 @@ def load_routes() -> RouteData | None:
     except FileNotFoundError:
         print(f"IOError: Couldn't open {data_path}")
         return None
+
+
+def lonlat_to_xy(win: GraphWin, lon: float, lat: float):
+    '''Written by Philip Mees for CMPT 103
+    Purpose: convert longitude/latitude locations to x/y pixel locations
+        This avoids the use of the setCoords, toWorld, and toScreen methods and graphics.py incompatibilities
+    Parameters:
+        win (GraphWin): the GraphWin object of the GUI
+        lon, lat (float): longitude and latitude to be converted
+    Returns: x, y (int): pixel location inside win'''
+
+    xlow, xhigh = -113.720049, -113.320418
+    ylow, yhigh = 53.657116, 53.393703
+
+    width, height = win.getWidth(), win.getHeight()
+
+    x = (lon - xlow) / (xhigh - xlow) * width
+    y = (lat - ylow) / (yhigh - ylow) * height
+
+    return int(x), int(y)
 
 
 def main() -> None:
